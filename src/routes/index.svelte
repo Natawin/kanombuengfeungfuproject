3e37540232c407b819c9e3e2af4b3367e4fba780
flokesong_drumer <87555423+FlokeZa89@users.noreply.github.com>
2022-07-31 15:24:31 +0700

    reupload

---
+++ src/routes/index.svelte
@@ -0,0 +1,30 @@
+ <html lang="en">
+    <head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+  
+      <!-- Title name -->
+      <title>ขนมเบื้องเฟื่องฟู</title>
+
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+  
+    </head>
+    <body>
+      <!-- font -->
+      <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=Prompt:wght@300&display=swap" rel="stylesheet">
+      <style>
+          body {
+              font-family: 'Prompt', sans-serif;
+          }
+      </style>
+  
+      <!-- Import bootstrap -->
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+  
+      <!-- On top navbar -->
+      หน้าเว็บไซต์ยังอยู่ระหว่างการพัฒนา!
+    </body>
+  </html>
+
